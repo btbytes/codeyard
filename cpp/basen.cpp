c3d92442543b38f0d877dcb3529f6a01e93e1d36
Pradeep Gowda <pradeep@btbytes.com>
2024-07-25 16:23:36 -0400

    add makefile

--- cpp/basen.cpp
+++ cpp/basen.cpp
@@ -1,10 +1,10 @@
-/* 
+/*
  * basen.cpp
  *
  * Convert a given integer n to base 64
  * using the digits lookup table
  * useful for generating short urls for example
- * in a URL shortener application 
+ * in a URL shortener application
  *
  * Pradeep Gowda
  * Jul 25, 2024
@@ -21,7 +21,12 @@ int main() {
 	int quotient;
 	int remainder;
 	std::list<char> rep;
-        char digits[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '-', '_'};
+        char digits[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+            'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w',
+            'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+            'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W',
+            'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+            '-', '_'};
 	while (curval >= b){
 		quotient = curval/b;
 		remainder = curval % b;
@@ -36,6 +41,6 @@ int main() {
 	return 0;
 }
 
-/* compile: 
- * 	g++ -Wc++11-extensions -std=c++17 basen.cpp -o basen 
+/* compile:
+ * 	g++ -Wc++11-extensions -std=c++17 basen.cpp -o basen
  * */
